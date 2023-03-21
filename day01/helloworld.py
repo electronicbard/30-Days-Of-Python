dbb00db8fe459368d733be43bfdaacdf7f6c1c07
electronicbard <emsolorio@gmail.com>
2023-03-21 15:26:19 -0700

    Day one first commit

---
+++ day01/helloworld.py
@@ -0,0 +1,58 @@
+#    1. Check the python version you are using
+
+python3 --version
+
+#    2. Open the python interactive shell and do the following operations. The operands are 3 and 4.
+#        addition(+)
+print(3 + 4)
+#        subtraction(-)
+print(3 - 4)
+#        multiplication(*)
+print(3 * 4)
+#        modulus(%)
+print(3 % 4)
+#        division(/)
+print(3 / 4)
+#        exponential(**)
+print(3 ** 4)
+#        floor division operator(//)
+print(3 // 4)
+#        Write strings on the python interactive shell. The strings are the following:
+#        Your name
+print('El Bardo')
+#        Your family name
+print('Bardino')
+#        Your country
+print('USA')
+#        I am enjoying 30 days of python
+print('I am enjoying 30 days of python')
+
+#    Check the data types of the following data:
+
+#        10
+print(type(10))
+#<class 'int'>
+
+#        9.8
+print(type(9.8))
+#<class 'float'>
+
+#        3.14
+print(type(3.14))
+#<class 'float'>
+
+#        4 - 4j
+print(type(4-4j))
+#<class 'complex'>
+#        ['Asabeneh', 'Python', 'Finland']
+print(type(['Bob', 'Luis', 'Predrag']))
+#<class 'list'>
+#        Your name
+print(type('El Bardo'))
+#<class 'str'>
+#        Your family name
+print(type('Bardino'))
+#<class 'str'>
+#        Your country
+print(type('U.S.A.'))
+#<class 'str'>
